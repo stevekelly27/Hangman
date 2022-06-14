fe89a68a9c222745c1b6a74ab23617a09003d864
stevekelly27 <stevekelly28@hotmail.com>
2022-06-14 00:41:55 +0000

    function pick_word is working and linked to requirements.txt

--- run.py
+++ run.py
@@ -1,3 +1,18 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+
+
+
+def pick_word():
+    """
+    generates a word from my_words.txt
+    """
+    my_words = open("requirements.txt", "r")
+    words = my_words.readlines()
+    my_words.close()
+
+    return random.choice(words)[:-1]
+pick_word()
+# def playing():
+#     word = pick_word(my_words):
+#     guess_letters =set(word)
+#     used_letters = set()
