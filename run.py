161f204ceef45a49071649db4341e168c1119ef1
stevekelly27 <stevekelly28@hotmail.com>
2022-06-14 20:35:17 +0000

    Added condition of 10 lives to win/loose the game

--- run.py
+++ run.py
@@ -20,19 +20,24 @@ def playing():
     word_letters = set(word)
     alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
     used_letters = set()
+    liuves = 10 
 
-    while len(word_letters) > 0:
+    while len(word_letters) > 0 and lives > 0:
 
         print('You have used the letters: ', ' '.join(used_letters))
+        print('You have', lives, 'lives left: ')
 
         current_word = [letter if letter in used_letters else '-' for letter in word]
         print('Current word: ', ' '.join(current_word))
-        
+
         guess_letter = input('Guess a letter: ')
         if guess_letter in alphabet:
             used_letters.add(guess_letter)
             if guess_letter in word_letters:
                 word_letters.remove(guess_letter)
+            else:
+                lives = lives -1
+                print('ooops, try again!')    
 
         elif guess_letter in used_letters:
             print('Character already in use. Please try again!') 
